e1de58d1de5b64d741d570f9673d08937bc6e7b4
Gabor Simon <simon.gabor87hu@gmail.com>
2021-04-11 09:07:56 +0200

    Code formatting

--- app/app.py
+++ app/app.py
@@ -1,21 +1,29 @@
 import boto3
 import psycopg2
 
-DEFAULT_REGION='eu-central-1'
+DEFAULT_REGION = 'eu-central-1'
 ssm = boto3.client('ssm', DEFAULT_REGION)
-ENDPOINT=ssm.get_parameter(Name='DB_ENDPOINT', WithDecryption=True)['Parameter']['Value']
-PORT=ssm.get_parameter(Name='DB_PORT', WithDecryption=True)['Parameter']['Value']
-USER=ssm.get_parameter(Name='DB_USER', WithDecryption=True)['Parameter']['Value']
-PASSWORD=ssm.get_parameter(Name='DB_PASSWORD', WithDecryption=True)['Parameter']['Value']
-REGION=ssm.get_parameter(Name='DB_REGION', WithDecryption=True)['Parameter']['Value']
-DBNAME=ssm.get_parameter(Name='DB_DBNAME', WithDecryption=True)['Parameter']['Value']
+ENDPOINT = ssm.get_parameter(Name='DB_ENDPOINT', WithDecryption=True)[
+    'Parameter']['Value']
+PORT = ssm.get_parameter(Name='DB_PORT', WithDecryption=True)[
+    'Parameter']['Value']
+USER = ssm.get_parameter(Name='DB_USER', WithDecryption=True)[
+    'Parameter']['Value']
+PASSWORD = ssm.get_parameter(Name='DB_PASSWORD', WithDecryption=True)[
+    'Parameter']['Value']
+REGION = ssm.get_parameter(Name='DB_REGION', WithDecryption=True)[
+    'Parameter']['Value']
+DBNAME = ssm.get_parameter(Name='DB_DBNAME', WithDecryption=True)[
+    'Parameter']['Value']
 
 try:
-    conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=PASSWORD)
+    conn = psycopg2.connect(host=ENDPOINT, port=PORT,
+                            database=DBNAME, user=USER, password=PASSWORD)
     cur = conn.cursor()
     cur.execute("""SELECT current_database(), current_user, version()""")
     query_results = cur.fetchall()
-    print(f"Connected to {query_results[0][0]} database with {query_results[0][1]} as user")
+    print(
+        f"Connected to {query_results[0][0]} database with {query_results[0][1]} as user")
     print(f"Version information: {query_results[0][2]}")
 except Exception as e:
     print(f"Database connection failed due to {e}")
