2cfc3af154e2845689a8db7d219bbf2d942d15db
Gabor Simon <simon.gabor87hu@gmail.com>
2021-04-14 15:27:45 +0200

    Modified EC2 setup script

--- EC2_setup/script.sh
+++ EC2_setup/script.sh
@@ -8,7 +8,6 @@ unzip awscliv2.zip
 sudo ./aws/install
 
 # Docker install
-
 sudo apt-get update
 sudo apt-get install -y \
 apt-transport-https \
@@ -23,21 +22,12 @@ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# Jenkins run
-sudo docker network create jenkins
+# Java install
+sudo apt install -y openjdk-11-jdk
 
-sudo docker run --name jenkins-docker --rm --detach \
---privileged --network jenkins --network-alias docker \
---env DOCKER_TLS_CERTDIR=/certs \
---volume jenkins-docker-certs:/certs/client \
---volume jenkins-data:/var/jenkins_home \
---publish 2376:2376 docker:dind --storage-driver overlay2
-
-sudo docker build -t myjenkins-blueocean:1.1 .
-sudo docker run --name jenkins-blueocean --rm --detach \
---network jenkins --env DOCKER_HOST=tcp://docker:2376 \
---env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
---publish 8080:8080 --publish 50000:50000 \
---volume jenkins-data:/var/jenkins_home \
---volume jenkins-docker-certs:/certs/client:ro \
-myjenkins-blueocean:1.1
+# Jenkins install
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y jenkins
